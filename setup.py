d2eae4d8bcf822b4ca06acb0ba6602af99bdd726
Martin Røvang <martinrovang@gmail.com>
2022-08-11 13:03:15 +0200

    pip install dependency fix attempt

--- setup.py
+++ setup.py
@@ -8,8 +8,10 @@
 """
 from setuptools import setup
 
+
 if __name__ == "__main__":
     try:
+        # setup(use_scm_version={"version_scheme": "no-guess-dev"})
         setup(use_scm_version={"version_scheme": "no-guess-dev"})
     except:  # noqa
         print(
